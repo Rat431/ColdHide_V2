896a395f8d0a7dcd543f77d2ab3993a6f3378e95
Rat431 <54189610+Rat431@users.noreply.github.com>
2020-08-07 22:04:41 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # ColdHide_V2
- A mini anti-anti debug hooking library for Windows
+ A mini anti-anti debug hooking library for Windows.
---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+/*
+	Copyright (c) 2019 Rat431 (https://github.com/Rat431).
+	This software is under the MIT license, for more informations check the LICENSE file.
+*/
+
+#include "hooks.h"
+
+// small export
+extern "C" _declspec(dllexport) void CHide_Bridge() { return; }
+
+BOOL APIENTRY DllMain( HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved) {
+	if (ul_reason_for_call == DLL_PROCESS_ATTACH) {
+		Hooks_Manager::Init(hModule);
+	}
+	else if (ul_reason_for_call == DLL_PROCESS_DETACH) {
+		Hooks_Manager::ShutDown();
+	}
+	return TRUE;
+}
+
